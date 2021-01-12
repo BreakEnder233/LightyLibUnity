3d3d77251dc5cf9acebcfa63fe88392523d8ef73
Break_Ender233 <Break_Ender233@163.com>
2021-01-12 11:19:28 +0800

    #2021_01_14 Add ordinary class singleton support @Lighty

---
+++ LightyLibUnity/Basic/Singleton.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LightyLibUnity.Basic
+{
+
+    /// <summary>
+    /// Used for all singleton MonoBehavior.
+    /// </summary>
+    /// <typeparam name="T">The class needed to be singleton.</typeparam>
+    public abstract class Singleton<T> where T : Singleton<T>
+    {
+        private static T _instance;
+        /// <summary>
+        /// Access to singleton instance.
+        /// </summary>
+        public static T Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+#if DEBUG
+                    Debug.LogError($"Instance of {typeof(T)} not initiated.");
+#else
+                throw SingletonAlreadyExistsException.CreateException(typeof(T));
+#endif
+                }
+                return _instance;
+            }
+            private set
+            {
+                _instance = value;
+            }
+        }
+        /// <summary>
+        /// Call in awake or earlier.
+        /// </summary>
+        protected void Init()
+        {
+            if (_instance == null) _instance = (T)this;
+            else
+            {
+#if DEBUG
+                Debug.LogError($"Instance of {typeof(T)} already existed!");
+#else
+                throw SingletonAlreadyExistsException.CreateException(typeof(T));
+#endif
+                return;
+            }
+        }
+
+        #region Convenience
+
+        #endregion
+    }
+
+}
