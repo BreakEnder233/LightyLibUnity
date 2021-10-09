4bd07f5e41cee9a5521cf47be4a28d3f352788e8
LightyTheLight <Break_Ender233@163.com>
2021-10-09 15:01:52 +0800

    Create Vector2Extension.cs

---
+++ LightyLibUnity/Extensions/Math/Vector2Extension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace LightyLibUnity.Extensions.Math
+{
+    public static class Vector2Extension
+    {
+        public static Vector3 MakeVector3XZ(this Vector2 vector2, float y = 0f)
+        {
+            return new Vector3(vector2.x, y, vector2.y);
+        }
+    }
+}
